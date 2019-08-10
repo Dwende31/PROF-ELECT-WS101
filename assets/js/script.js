72c14c3bef23f9561e2c8f91f48d5879a41bc54b
dwende31 <edkriztalmaes@gmail.comgit config --global user.name dwende31>
2019-08-11 04:46:48 +0800

    blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 function light(sw) {
     var pic;
-    if (sw == 0) {
-      pic = "../img/pic_bulboff.gif"
+    if (sw ==0 ) {
+      pic = "./assets/img/pic_bulboff.gif"
     } else {
-      pic = "../img/pic_bulbon.gif"
+      pic = "./assets/img/pic_bulbon.gif"
     }
     document.getElementById('myImage').src = pic;
   }
